843fab02b5a00a75674f856d292892427b73c7f3
HHXX88 <74294767+HHXX88@users.noreply.github.com>
2020-11-25 09:53:50 +0800

    Create 112502.QS排名查询.app.c

---
+++ 112502.QS排名查询.app.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("QS大学排名查询\n");
+printf("请输入排名：");
+scanf("%d",&a);
+if (a==1)
+{
+printf("1.广西科技师范学院\n");
+}
+else if (a==2)
+{
+printf("%d.排名第%d的大学\n",a,a);
+}
+else if (a==3)
+{
+printf("%d.排名第%d的大学\n",a,a);
+}
+else if (a==4)
+{
+printf("%d.排名第%d的大学\n",a,a);
+}
+else if (a==5)
+{
+printf("%d.排名第%d的大学\n",a,a);
+}
+else if (a==6)
+{
+printf("%d.排名第%d的大学\n",a,a);
+}
+else if (a==7)
+{
+printf("%d.排名第%d的大学\n",a,a);
+}
+else if (a==8)
+{
+printf("%d.排名第%d的大学\n",a,a);
+}
+else if (a==9)
+{
+printf("%d.排名第%d的大学\n",a,a);
+}
+else if (a==10)
+{
+printf("%d.排名第%d的大学\n",a,a);
+}
+else
+{
+printf("老铁的排名太靠后了，请输入1-10\n");
+}
+}
