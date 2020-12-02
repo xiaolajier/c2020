e9a1660efec5a2ebbfd90bdc179134015a428c4a
HHXX88 <74294767+HHXX88@users.noreply.github.com>
2020-12-02 17:36:03 +0800

    Update 120204.数据类型的转换.app.c

--- 120204.数据类型的转换.app.c
+++ 120204.数据类型的转换.app.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 void main()
 {
-	double x;
+	double x,y;
 	printf("欢迎使用*小数取整小程序*：请输入...\n");
 	scanf("%lf",&x);
-	printf("转换结果为\n%d\n",(int)x);
+	y=x;
+	printf("转换结果为\n%d\n",(int)y);
 }
